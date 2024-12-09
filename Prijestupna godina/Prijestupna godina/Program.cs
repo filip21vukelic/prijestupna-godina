ac82935ecb527fe566fc59bceebed6599087e530
filip21vukelic <vukelicfilip1@gmail.com>
2024-12-09 13:01:30 +0100

    Add files via upload

---
+++ Prijestupna godina/Prijestupna godina/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prijestupna_godina
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int godina = 1900; godina <= 2100; godina++)
+            {
+                if ((godina % 400 == 0) || (godina % 4 == 0 && godina % 100 != 0))
+                {
+                    Console.WriteLine(godina);
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
